3091010bc305a067b624378844505e7eec6a0c44
Gregory Hunkeler <gregoryhunkeler@gmail.com>
2023-03-16 10:37:52 -0300

    important

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -18,6 +18,12 @@ function Home() {
         createdAt. En el campo artistName se guarda el nombre se haya encontrado
         resultado o no y por lo tanto también se guardan los otros datos.
       </p>
+      <h5 className="p-md-5 important">
+        <b> Importante:</b> En el back se debe proporcionar el ClientId y el
+        ClientIdSecret obtenidos en
+        https://developer.spotify.com/dashboard/login ya que se eligió como
+        Authorization Code Flow el flujo "Client credentials".
+      </h5>
     </div>
   );
 }
